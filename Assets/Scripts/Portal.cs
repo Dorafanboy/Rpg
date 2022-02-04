187e8c70ac25baa07140fbc0e5bbc8b242ad1546
Dorafanboy <73462832+Dorafanboy@users.noreply.github.com>
2022-02-04 09:52:20 +0700

    all project

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : Collidable
+{   
+    public string[] sceneNames;
+    protected override void OnCollide(Collider2D coll)
+    {
+        if(coll.name == "Player_0")
+        {
+            GameManager.instance.SaveState();
+            string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
